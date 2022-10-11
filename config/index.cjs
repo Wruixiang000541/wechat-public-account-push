c2ab58c260f93eb98cd3a6706371babef695c2a5
Wruixiang000541 <115529384+Wruixiang000541@users.noreply.github.com>
2022-10-11 15:30:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,20 +23,20 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKjaa5miZTVchXZ95SM-vnR7XdLs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'yEhONUe1azYl5iLgDaLTkNMKkravK65BUvtPHvgo2ZE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-07',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '王瑞翔', year: '1999', date: '06-07',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '王瑞翔', year: '1999', date: '07-19',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '纪念日', name: '纪念日', year: '2022', date: '09-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
